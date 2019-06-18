e792eea22f34a1b0680e90aede140b1a70cc94db
Rodrigo Pena <rodrigo.pena@epfl.ch>
2019-06-18 16:59:23 +0200

    add test_install.py

---
+++ test_install.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""Test installation by importing all the required modules."""
+
+import matplotlib as mpl
+import networkx as nx
+import numpy as np
+import pandas as pd
+import pathos.multiprocessing as mp
+import PIL
+import pyflann
+import pygsp
+import pyunlocbox
+import skimage
+import scipy
+import tqdm
+
+if __name__ == "__main__":
+    
+    print("Installation successful!")
+    
+    
