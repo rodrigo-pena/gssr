e7ceca81923d952a37b3a9fc3f604bb6b443f7be
Rodrigo Pena <rodrigo.pena@epfl.ch>
2019-06-21 09:05:19 +0200

    delete intermediate experiment files and update run experiment code

--- phd-thesis/run_experiment_4.sh
+++ phd-thesis/run_experiment_4.sh
@@ -9,16 +9,16 @@ export OMP_NUM_THREADS=1
 
 # BSDS300 #
 #echo "BSDS300"
-#python3 pt_bsds.py -sub 'both' -gtype 'grid_and_patches' -nt 15 -nm 25 -sd "jump_set_tv_coherence" -rf "tv_interpolation" -fn "pt_bsds300_jump_set_coherence_samp_tv_interp"
+python3 pt_bsds.py -sub 'test' -gtype 'grid_and_patches' -nt 15 -nm 25 -sd "jump_set_tv_coherence" -rf "tv_interpolation" -fn "pt_bsds300_jump_set_coherence_samp_tv_interp"
 
 # 2-SSBM #
-echo "2-SSBM"
-python3 pt_ssbm.py -nv 1000 -nc 2 -nt 25 -b 0.5 -na 51 -nm 51 -sd "jump_set_tv_coherence" -rf "tv_interpolation" -fn "pt_2ssbm_jump_set_coherence_samp_tv_interp"
+#echo "2-SSBM"
+#python3 pt_ssbm.py -nv 1000 -nc 2 -nt 25 -b 0.5 -na 51 -nm 51 -sd "jump_set_tv_coherence" -rf "tv_interpolation" -fn "pt_2ssbm_jump_set_coherence_samp_tv_interp"
 
 # Unbalanced 2-SBM #
-echo "Unbalanced 2-SBM"
-python3 pt_sbm.py -nv 1000 -nc 2 -nvpc 200 800 -nt 25 -b 0.5 -na 51 -nm 51 -sd "jump_set_tv_coherence" -rf "tv_interpolation" -fn "pt_2sbm_jump_set_coherence_samp_tv_interp"
+#echo "Unbalanced 2-SBM"
+#python3 pt_sbm.py -nv 1000 -nc 2 -nvpc 200 800 -nt 25 -b 0.5 -na 51 -nm 51 -sd "jump_set_tv_coherence" -rf "tv_interpolation" -fn "pt_2sbm_jump_set_coherence_samp_tv_interp"
 
 # email-EU-core #
-echo "email-EU-core"
-python3 pt_email_eu_core.py -nc 5 -nt 51 -nm 101 -sd "jump_set_tv_coherence" -rf "tv_interpolation" -fn "pt_email_eu_core_jump_set_coherence_samp_tv_interp"
+#echo "email-EU-core"
+#python3 pt_email_eu_core.py -nc 5 -nt 51 -nm 101 -sd "jump_set_tv_coherence" -rf "tv_interpolation" -fn "pt_email_eu_core_jump_set_coherence_samp_tv_interp"
